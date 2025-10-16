861364e5db6d911d0c2262f2f470d3230e6dac25
Mariyamathul Kaizarana <4pa23ic028@pace.edu.in>
2025-10-16 06:10:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9355547405-2719c",
+  "appId": "1:726103739091:web:b032602808e4e37ff96226",
+  "apiKey": "AIzaSyBKZdg8iKcdBXdl6grqgSp6QXOje56y5S4",
+  "authDomain": "studio-9355547405-2719c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "726103739091"
+};
